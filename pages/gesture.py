f101c5ee241afa70a85fad3370dcde27abcb7511
Unnati Kadam <74922646+unnatikdm@users.noreply.github.com>
2025-04-26 18:52:49 +0530

    Update gesture.py

--- pages/gesture.py
+++ pages/gesture.py
@@ -126,15 +126,8 @@ st.markdown('<p class="title">✋ Hand Gesture Detection</p>', unsafe_allow_html
 
 # --- Get Video File ---
 # First try session state
-video_path = st.session_state.get("uploaded_video_path")
+video_path = st.session_state.get("uploaded_video_path", "uploaded_videos/converted-video.mp4")
 
-# If session reset, try reading last saved video path
-if not video_path or not os.path.exists(video_path):
-    try:
-        with open("uploaded_videos/last_video.txt", "r") as f:
-            video_path = f.read().strip()
-    except:
-        video_path = None
 
 if not video_path or not os.path.exists(video_path):
     st.warning("⚠️ No video found! Please upload a video in the Interview Page.")
